4a146bf732e970af6d88041bda18c12aeb978e38
Omar Momani <o.a.momani@gmail.com>
2022-06-12 10:45:50 +0300

    Weird String Problem

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ const toWeirdCase = (string) =>
   [...string]
     .map((e, i) => (i % 2 ? e.toLowerCase() : e.toUpperCase()))
     .join("");
+
+simplifiedWeird = (str) => {
+  return [...str]
+    .map((e, i) => {
+      // spread operator to copy the array of the string
+      if (i % 2) {
+        //  if the index of string % 2 = 0 will convert the first letter to LowerCase
+        return e.toLowerCase(); // Map Function to creeate new array
+      } else {
+        return e.toUpperCase(); // else to UpperCase
+      }
+    })
+    .join("");
+};
