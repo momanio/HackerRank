ceacbdeb63ef21182084af317afab20cf49100e6
Omar Momani <o.a.momani@gmail.com>
2022-06-19 14:56:49 +0300

    WeirdString from CodeWare

--- WiErDsTrInG/app.js
+++ WiErDsTrInG/app.js
@@ -1,16 +1,12 @@
-const toWeirdCase = (string) =>
-  [...string]
-    .map((e, i) => (i % 2 ? e.toLowerCase() : e.toUpperCase()))
-    .join("");
-
-simplifiedWeird = (str) => {
+//const toWeirdCase=(string)=>[...string].map((e,i)=>i%2?e.toLowerCase():e.charAt(0).toUpperCase()).join(''); //
+const toWeirdCase = (str) => {
   return [...str]
     .map((e, i) => {
       // spread operator to copy the array of the string
       if (i % 2) {
         //  if the index of string % 2 = 0 will convert the first letter to LowerCase
         return e.toLowerCase(); // Map Function to creeate new array
-      } else {
+      } else if (i % 2) {
         return e.toUpperCase(); // else to UpperCase
       }
     })
