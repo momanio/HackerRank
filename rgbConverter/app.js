20605a73bb91f5daf9960b62947f90eb3402cecf
Omar Momani <o.a.momani@gmail.com>
2022-06-21 01:15:13 +0300

    rgb to hex converter problem from codewars edited

--- rgbConverter/app.js
+++ rgbConverter/app.js
@@ -8,3 +8,17 @@ const toHex = (value) => {
 
 //console.log(toHex(240))
 console.log(rgb(20, 194, 110));
+
+//Short Version of hexColour function from MDN
+
+// The old version
+function oldHexColour(c) {
+  if (c < 256) {
+    return Math.abs(c).toString(16);
+  }
+  return 0;
+}
+//The es6 arrow function version
+const hexColour = (c) => {
+  return c < 256 ? Math.abs(c).toString(16) : 0;
+};
