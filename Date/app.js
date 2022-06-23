0ef25e53a5126f59d04e2c9534a73e14bda07c56
Omar Momani <o.a.momani@gmail.com>
2022-06-24 01:02:02 +0300

    Date Problem

---
+++ Date/app.js
@@ -0,0 +1,19 @@
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+  let dayName;
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  // Write your code here
+  const date = new Date(dateString);
+
+  dayName = days[date.getDay()];
+
+  return dayName;
+}
